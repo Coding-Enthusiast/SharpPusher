809f3ab1cd1ae710fe05df6bf2b43c0c8b5632ff
Yokomoko <yokomoko_chapster@live.co.uk>
2019-01-14 09:25:22 +0000

    installer + small fix

--- SharpPusher/Properties/AssemblyInfo.cs
+++ SharpPusher/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SharpPusher")]
+[assembly: AssemblyTitle("Groestlcoin Broadcaster")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Coding Enthusiast Coders")]
-[assembly: AssemblyProduct("SharpPusher")]
-[assembly: AssemblyCopyright("Copyright © C.E. 2017")]
+[assembly: AssemblyCompany("Yokomoko")]
+[assembly: AssemblyProduct("Groestlcoin Broadcaster")]
+[assembly: AssemblyCopyright("Copyright © C.E. 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.10.0.0")]
-[assembly: AssemblyFileVersion("0.10.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

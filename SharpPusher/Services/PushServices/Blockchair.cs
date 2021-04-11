1122a05fa82260febc952774029be62cc105f84f
Angelo M <70480326+AngeloMetal@users.noreply.github.com>
2021-04-11 11:07:55 +0300

    Add files via upload

--- SharpPusher/Services/PushServices/Blockchair.cs
+++ SharpPusher/Services/PushServices/Blockchair.cs
@@ -17,7 +17,24 @@ namespace SharpPusher.Services.PushServices
         public enum Chain
         {
             BTC,
-            BCH
+            BCH,
+            DOGE,
+            LTC,
+            XMR,
+            TBTC,
+            BSV,
+            ZEC,
+            XRP,
+            tETH,
+            ETH,
+            EOS,
+            XTZ,
+            XIN,
+            ADA,
+            XLM,
+            GRS,
+            DASH,
+            ABC
         }
 
         private readonly Chain chain;
@@ -32,7 +49,64 @@ namespace SharpPusher.Services.PushServices
             {
                 try
                 {
-                    string chainName = chain == Chain.BTC ? "bitcoin" : "bitcoin-cash";
+                    string chainName = "";
+                    if(chain == Chain.BTC)
+                    {
+                        chainName = "bitcoin";
+                    }else if(chain == Chain.TBTC)
+                    {
+                        chainName = "bitcoin/testnet";
+                    }else if(chain == Chain.BCH)
+                    {
+                        chainName = "bitcoin-cash";
+                    }else if(chain == Chain.DOGE)
+                    {
+                        chainName = "dogecoin";
+                    }else if(chain == Chain.LTC)
+                    {
+                        chainName = "litecoin";
+                    }else if(chain == Chain.XMR)
+                    {
+                        chainName = "monero";
+                    }else if(chain == Chain.ADA)
+                    {
+                        chainName = "cardano";
+                    }else if(chain == Chain.BSV)
+                    {
+                        chainName = "bitcoin-sv";
+                    }else if(chain == Chain.EOS)
+                    {
+                        chainName = "eos";
+                    }else if(chain == Chain.ETH)
+                    {
+                        chainName = "ethereum";
+                    }else if(chain == Chain.tETH)
+                    {
+                        chainName = "ethereum/testnet";
+                    }else if(chain == Chain.XIN)
+                    {
+                        chainName = "mixin";
+                    }else if(chain == Chain.XLM)
+                    {
+                        chainName = "stellar";
+                    }else if(chain == Chain.XRP)
+                    {
+                        chainName = "ripple";
+                    }else if(chain == Chain.XTZ)
+                    {
+                        chainName = "tezos";
+                    }else if (chain == Chain.DASH)
+                    {
+                        chainName = "dash";
+                    }else if (chain == Chain.GRS)
+                    {
+                        chainName = "groestlcoin";
+                    }else if (chain == Chain.ABC)
+                    {
+                        chainName = "bitcoin-abc";
+                    }
+
+
                     string url = $"https://api.blockchair.com/{chainName}/push/transaction";
 
                     var content = new FormUrlEncodedContent(new[]
